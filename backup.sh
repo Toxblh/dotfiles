2b3767731dae0428e9786650a587a874f35c9691
Anton Palgunov <toxblh@gmail.com>
2020-08-07 13:01:38 +0100

    added home folder config

--- backup.sh
+++ backup.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+
+# ~/.config
 input="./config.conf"
 while IFS= read -r line
 do
   echo "$line"
   cp -R ~/.config/$line ./.config/
+done < "$input"
+
+# ~
+input="./home.conf"
+while IFS= read -r line
+do
+  echo "$line"
+  cp -R ~/$line ./.home/
 done < "$input"
