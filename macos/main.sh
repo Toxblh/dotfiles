4d819ce3ed158e5f97219a4868fb54014c0e4268
Anton Palgunov <toxblh@gmail.com>
2022-11-10 14:37:11 +0000

    upd

--- macos/main.sh
+++ macos/main.sh
@@ -5,13 +5,13 @@ fi
 
 cp ./configs/.gitconfig ~/.gitconfig
 
-# ./brew/apps.sh
+./brew/apps.sh
 
-# ./zsh.sh
-# # check zsh.sh status. 0 - ok, 1 - need restart
-# if [[ $? -eq 1 ]]; then
-#     exit 0
-# fi
+./zsh.sh
+# check zsh.sh status. 0 - ok, 1 - need restart
+if [[ $? -eq 1 ]]; then
+    exit 0
+fi
 
-# ./brew/rust.sh
+./brew/rust.sh
 ./brew/nvm.sh
